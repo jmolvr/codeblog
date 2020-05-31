3b7ad9f5bfebd1416dd41efa37cb0aa2c208c016
João Marcos <joaosantana1998@gmail.com>
2020-05-31 02:27:41 -0300

    First Commit

---
+++ src/main/java/com/spring/codeblog/repository/CodeBlogRepository.java
@@ -0,0 +1,9 @@
+package com.spring.codeblog.repository;
+
+import com.spring.codeblog.model.Post;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CodeBlogRepository extends JpaRepository<Post, Long> {
+    
+}
